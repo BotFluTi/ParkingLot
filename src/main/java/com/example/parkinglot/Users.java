7a03f3a26afe5299b8835d9bb19f6028bffb45e2
BotFluTi <162293866+BotFluTi@users.noreply.github.com>
2024-12-29 19:15:29 +0200

    Added a stateful EJB and a file upload

--- src/main/java/com/example/parkinglot/Users.java
+++ src/main/java/com/example/parkinglot/Users.java
@@ -1,14 +1,17 @@
 package com.example.parkinglot;
 
 import com.example.parkinglot.common.UserDto;
+import com.example.parkinglot.ejb.InvoiceBean;
 import com.example.parkinglot.ejb.UserBean;
+import jakarta.annotation.security.DeclareRoles;
 import jakarta.inject.Inject;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
-import jakarta.annotation.security.DeclareRoles;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @DeclareRoles({"READ_USERS", "WRITE_USERS"})
@@ -19,17 +22,34 @@ public class Users extends HttpServlet {
 
     @Inject
     UserBean userBean;
+    @Inject
+    InvoiceBean invoiceBean;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse
             response) throws ServletException, IOException {
         List<UserDto> users = userBean.findAllUsers();
         request.setAttribute("users", users);
+
+        if(!invoiceBean.getUserIds().isEmpty()) {
+            Collection<String> usernames = userBean.findUsernamesByUserIds(invoiceBean.getUserIds());
+            request.setAttribute("invoices", usernames);
+        }
+
         request.getRequestDispatcher("/WEB-INF/pages/users.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse
             response) throws ServletException, IOException {
+        String[] userIdsAsString = request.getParameterValues("user_ids");
+        if(userIdsAsString != null) {
+            List<Long> userIds = new ArrayList<>();
+            for(String userIdAsString : userIdsAsString) {
+                userIds.add(Long.parseLong(userIdAsString));
+            }
+            invoiceBean.getUserIds().addAll(userIds);
+        }
+        response.sendRedirect(request.getContextPath() + "/Users");
     }
 }
